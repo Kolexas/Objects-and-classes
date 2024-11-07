5d78ae299d162f80c5f70dace3afd1c61c97aed0
Kolexas <maksimilyan.klimentev39@gmail.ru>
2024-11-07 17:46:40 +0300

    First Edition

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,23 @@
 public class Book {
+    private  String name;
+    private  Author author;
+    private int publicationDate;
+
+    public Book(String name, Author author, int publicationDate) {
+        this.name = name;
+        this.author = author;
+        this.publicationDate = publicationDate;
     }
+    public String getName() {
+        return this.name;
+    }
+    public Author getAuthor() {
+        return this.author;
+    }
+    public int publishingYear() {
+        return this.publicationDate;
+    }
+    public void setPublicationDate(int publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
 public class Main {
-    }
+  public static void main(String[] args) {
+    Author author = new Author("Лев","Толстой");
+    Book book = new Book("Война и мир", author, 1865);
+    Author author2 = new Author("Александр", "Пушкин");
+     Book book2 = new Book("Евгений Онегин", author2, 1825);
+     book2.setPublicationDate(1866);
+  }
+}
