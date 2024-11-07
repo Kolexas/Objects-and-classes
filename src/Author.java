22cf23738274e75332cf984d3caa64cd86c501fa
Kolexas <maksimilyan.klimentev39@gmail.ru>
2024-11-07 17:48:33 +0300

    First Edition

--- src/Author.java
+++ src/Author.java
@@ -1,20 +1,4 @@
 public class Author {
-    private String nameAuthor;
-     private String surname;
-
-    public Author(String name, String surname) {
-        this.nameAuthor = name;
-        this.surname = surname;
-    }
-    public String getName() {
-        return this.nameAuthor;
-    }
-    public String getSurname() {
-        return this.surname;
-    }
-        public String toString() {
-            return nameAuthor + " " + surname;
-        }
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,4 @@
 public class Main {
   public static void main(String[] args) {
-    Author author = new Author("Лев","Толстой");
-    Book book = new Book("Война и мир", author, 1865);
-    Author author2 = new Author("Александр", "Пушкин");
-     Book book2 = new Book("Евгений Онегин", author2, 1825);
-     book2.setPublicationDate(1866);
   }
 }
